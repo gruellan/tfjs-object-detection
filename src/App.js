eb44ca27ebcaa059f549370af29d32d2d2a03718
gruellan <ruellan.george@gmail.com>
2020-07-12 14:37:19 +0100

    Add tensorflow and detect 1 object in a picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '@tensorflow/tfjs'
+import * as cocoSsd from '@tensorflow-models/coco-ssd'
 
-function App() {
+function App () {
+  const [predictions, setPredictions] = useState([])
+
+  useEffect(() => {
+    run()
+  })
+
+  async function run () {
+    const img = document.getElementById('img')
+    const model = await cocoSsd.load()
+
+    // Classify the image
+    const res = await model.detect(img)
+    setPredictions({ class: res[0].class, score: res[0].score })
+  }
+
+  function renderResults () {
+    if (!isNaN(predictions.score)) {
+      return (
+        <div>
+          {(predictions.score * 100).toFixed(2) + '%'}
+        </div>
+      )
+    } else {
+      return <div>Loading...</div>
+    }
+  }
+
   return (
-    <div className="app">
-        <p>
-          yo yo
-        </p>
+    <div className='app'>
+      <p>yo yo</p>
+      <img id='img' alt='img' src='frank.jpg' />
+      {renderResults()}
     </div>
   )
 }
